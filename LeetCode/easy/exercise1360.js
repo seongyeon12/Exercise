06301f5abcf3681015650d690e6693a33efea4fc
seongyeon12 <k.seongyeon12@gmail.com>
2022-10-08 21:23:54 +0900

    2022/10/08

--- LeetCode/easy/exercise1360.js
+++ LeetCode/easy/exercise1360.js
@@ -4,5 +4,5 @@ var daysBetweenDates = function(date1, date2) {
       
     return Math.abs((oldDays.getTime() - now.getTime()) / 86400000);
   };
-
+ 
   console.log(daysBetweenDates("2022-02-05", "2022-05-08"))
