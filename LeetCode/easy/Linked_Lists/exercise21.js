d156c495328a33bbf378aea8c1ac88eaf02b8fe3
seongyeon12 <k.seongyeon12@gmail.com>
2022-10-16 20:32:40 +0900

    2022/10/16

--- LeetCode/easy/Linked_Lists/exercise21.js
+++ LeetCode/easy/Linked_Lists/exercise21.js
@@ -1,7 +1,40 @@
-var mergeTwoLists = function(list1, list2) {
-  let count = 0;
-  
-  while (count < 2) {
-      brea
+var mergeTwoLists = function(l1, l2) {
+  if (!l1) {
+    return l2
+  } else if(!l2) {
+    return l1
   }
-};
+  let list = new ListNode()
+
+  if (l1.val <= l2.val) {
+    list.val = l1.val 
+    l1 = l1.next
+  } else {
+    list.val = l2.val 
+    l2 = l2.next
+  }
+
+  let cur = list;
+
+  while (l1 && l2) {
+    if (l1.val <= l2.val) {
+      let node = new ListNode(l1.val, null)
+      cur.next = node
+      cur = cur.next
+      l1 = l1.next
+    } else {
+      let node = new ListNode(l2.val, null)
+      cur.next = node
+      cur = cur.next
+      l2 = l2.next
+    }
+  }
+
+  if (!l1) {
+    cur.next = l2
+  } else {
+    cur.next = l1
+  }
+
+  return list
+}
